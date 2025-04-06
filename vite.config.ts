ce93c718093199b3a82d83d9eb0061d629390fae
Ravi teja <raviteja@Ravis-iMac.local>
2025-04-06 22:23:55 +0530

    originnn

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx']
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+    extensions: ['.tsx', '.ts', '.jsx', '.js']
   },
   build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-      },
-    },
     outDir: 'dist',
-    emptyOutDir: true,
-    chunkSizeWarningLimit: 1000, // Increase the warning limit to 1000kb
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'lottie-player': ['@dotlottie/player-component'],
-        }
-      }
-    }
+    sourcemap: true
   }
 });
